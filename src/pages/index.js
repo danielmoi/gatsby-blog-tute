9d6122cb80de5fc284a8fec073b0f1de32ab2168
Daniel Moi <dmoixy@gmail.com>
2018-10-27 14:13:49 +1100

    Extract Header into /components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,6 @@
 import React from "react"
 
-// Get data
-import { StaticQuery, graphql } from 'gatsby';
-const TitleAndDescription = ({ data }) => {
-  const { title, description } = data.site.siteMetadata;
-  return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </div>
-  )
-}
-
-const Header = () => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query {
-          site {
-            siteMetadata {
-              title
-              description
-            }
-          }
-        }
-      `}
-      render={data => <TitleAndDescription data={data} />}
-    />
-  )
-}
+import Header from '../components/Header';
 
 const Layout = () => {
   return (
