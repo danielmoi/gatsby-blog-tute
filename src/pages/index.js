afaa66c559b74b44bf101aaae88b6d7c4f7064ba
Daniel Moi <dmoixy@gmail.com>
2018-10-27 14:10:29 +1100

    Get data from GraphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,46 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+// Get data
+import { StaticQuery, graphql } from 'gatsby';
+const TitleAndDescription = ({ data }) => {
+  const { title, description } = data.site.siteMetadata;
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              title
+              description
+            }
+          }
+        }
+      `}
+      render={data => (
+        <div>
+          {data.site.siteMetadata.title}
+        </div>
+      )}
+    />
+  )
+}
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Layout;
+
