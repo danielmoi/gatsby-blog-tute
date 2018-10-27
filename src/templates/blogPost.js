e996be25737bda89bbb138e12841ba461a84842e
Daniel Moi <dmoixy@gmail.com>
2018-10-27 15:18:43 +1100

    Get HTML from graphql and render blog posts!

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -2,11 +2,32 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 const Template = props => {
+  const { title } = props.data.markdownRemark.frontmatter;
+  const { html } = props.data.markdownRemark;
   return (
     <div>
-      Blog Post here...
+      <h1>{title}</h1>
+      <div
+        className="blogpost"
+        dangerouslySetInnerHTML={{ __html: html }}
+        style={{
+          fontFamily: 'avenir'
+        }}
+      />
+
     </div>
   )
 }
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
 
 export default Template;
