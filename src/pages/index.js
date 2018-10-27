8763d88a928867b4b552accdd7bc493e04d80b69
Daniel Moi <dmoixy@gmail.com>
2018-10-27 14:23:13 +1100

    Add query for all posts, and sort

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,46 @@
 import React from "react"
+import { graphql } from "gatsby";
 
 import Header from '../components/Header';
 
-const Layout = () => {
+const Layout = ({ data }) => {
+  const { edges } = data.allMarkdownRemark;
+
   return (
     <div>
       <Header />
+      {edges.map(edge => {
+        const { frontmatter } = edge.node;
+        return (
+          <div key={frontmatter.path}>{frontmatter.title}</div>
+        )
+      })}
     </div>
   )
 }
+
+export const query = graphql`
+  query HomepageQuery {
+    allMarkdownRemark (
+      sort: {
+        order: DESC,
+        fields: [
+          frontmatter___date
+        ]
+      }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Layout;
 
